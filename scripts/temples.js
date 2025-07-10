64034a1d5c3a8eb0cf968b501b92399b2ac60521
johnvak01 <johnvak01@gmail.com>
2025-07-09 18:21:25 -0600

    added JS functionality for Menu Button

---
+++ scripts/temples.js
@@ -0,0 +1,18 @@
+const currentyear_span = document.getElementById("currentyear");
+const lastmodified_p = document.getElementById("lastmodified");
+
+let this_year = new Date();
+
+currentyear_span.innerHTML = this_year.getFullYear();
+lastmodified_p.innerHTML = document.lastModified;
+
+// Hamburger menu code
+
+const menu_button = document.getElementById("menu");
+const nav_menu = document.querySelector("nav");
+
+menu_button.addEventListener('click',()=>{
+    menu_button.classList.toggle("open");
+    nav_menu.classList.toggle("visible");
+})
+
