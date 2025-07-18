d3ece9db6326893c282a88881f6db69a90068a1a
johnvak01 <johnvak01@gmail.com>
2025-07-17 18:27:04 -0600

    updated responsive page. created place files and basic structure

---
+++ scripts/place.js
@@ -0,0 +1,10 @@
+// Footer Code
+const currentyear_span = document.getElementById("currentyear");
+const lastmodified_p = document.getElementById("lastmodified");
+
+let this_year = new Date();
+
+currentyear_span.innerHTML = this_year.getFullYear();
+lastmodified_p.innerHTML = document.lastModified;
+
+//Responsive Code
